ce5713beabb2561acb44a8548ab7311decfc648c
JHandy24 <jmhandy@quinnipiac.edu>
2021-02-17 17:44:30 -0500

    Changed money input to an int for one dollar bills. Laid the framework for checking the item and proving change/refunds.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -16,8 +16,44 @@ public class VendingMachine {
 		
 		String purchase = input.nextLine();
 		
-		System.out.println("Please insert money by typing in a number with two decimals.");
+		System.out.println("Please input the number of one dollar bills you want to insert.");
 		
-		Double moneyInput = input.nextDouble();
+		int moneyInput = input.nextInt();
+		
+		System.out.println("Are you sure you want to make this purchase? Y for yes, N for no.");
+		String confirmation = input.nextLine();
+		
+		if(confirmation == "Y")
+		{
+			if (input == "C") {
+				if (moneyInput == chips) {
+					
+				}
+				else if(moneyInput > chips) {
+					
+				}
+				else {
+					System.out.println("You did not insert the required amount of money.")
+				}
+			}
+			else if (input == "CB") {
+				
+			}
+			else if (input == "G") {
+				
+			}
+			else if (input == "P") {
+				
+			}
+			else if (input == "POP") {
+				
+			}
+			else {
+				
+			}
+		}
+		else {
+			System.out.println("Your purchase has been cancelled and refunded. Here is your " + moneyInput + " dollars. Have a great day.");
+		}
 	}
 }
